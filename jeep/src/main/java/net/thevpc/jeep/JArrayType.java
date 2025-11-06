62d24e1564342fb53e39ea510cecb2fe603d0c2d
thevpc <taha.bensalah@gmail.com>
2025-11-06 22:49:47 +0100

    Update JArrayType.java : wip-advanced-types

--- jeep/src/main/java/net/thevpc/jeep/JArrayType.java
+++ jeep/src/main/java/net/thevpc/jeep/JArrayType.java
@@ -1,9 +1,9 @@
-package net.thevpc.jeep;
-
-public interface JArrayType extends JType {
-    int arrayDimension();
-    JType rootComponentType();
-    JType componentType();
-    JArray asArray(Object o);
-    Object newArray(int... len);
-}
+//package net.thevpc.jeep;
+//
+//public interface JArrayType extends JType {
+//    int arrayDimension();
+//    JType rootComponentType();
+//    JType componentType();
+//    JArray asArray(Object o);
+//    Object newArray(int... len);
+//}
